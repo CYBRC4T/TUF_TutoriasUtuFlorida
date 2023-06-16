08645f8319bdc94ed44371c50aacbc5d310834c2
CYBERC4T <136350968+CYBRC4T@users.noreply.github.com>
2023-06-16 11:06:37 -0300

    Add files via upload

--- tutorias/index.php
+++ tutorias/index.php
@@ -1,7 +1,8 @@
 <html>
 
     <head>
-        <title> TUTORIAS | PÁGINA PRINCIPAL </title>
+        <link rel="icon" href="img/logo.png">
+        <title> TUF | PÁGINA PRINCIPAL </title>
         <link rel="stylesheet" href="css/estilo1.css">
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,0,0" />
@@ -14,17 +15,20 @@
             <div class="divisor"></div>
             <img src="img/utu.png">
             <div class="divisor"></div>
-            <p> TUTORIAS </p>
+            <p> Tutorias UTU Florida </p>
         </nav>
         <div class="container">
             <div class="ventanas">
-                <a id="boton" class="ventanas" onclick="buttons()" href="#"> TUTORIAS </a>
-                <a id="boton" class="ventanas2" onclick="buttons()" href="#"> MATERIAS </a>
+                <a id="boton" class="ventanas" onclick="toggleButton(this)" href="#"> TUTORIAS </a>
+                <a id="boton2" class="ventanas2" onclick="toggleButton(this)" href="#"> MATERIAS </a>
                 <div class="filler"></div>
             </div>
-            <div class="materias">
-                <section>
+            <div class="pageSwitch">
+                <section class="tutorias">
                     <h3> ¿No ves la tutoría que quieres? Has click <a href="#">aquí</a> para solicitarla. </h3>
+                </section>
+                <section class="materias">
+                    
                 </section>
 
                 <aside>
@@ -35,12 +39,55 @@
             <footer>
             </footer>
     </body>
-    <script>
-        document.getElementById("boton").addEventListener("click", myScript);
 
-        document.getElementById('boton').onclick = function(){
-            
-            element.class = (element.class == "opacityClicked") ? "opacityDefault" : "opacityClicked";
+    <script type="text/javascript">
+        function hasClass(element, className) {
+            return element.classList.contains(className);
+        }
+
+        function addClass(element, className) {
+            element.classList.add(className);
+        }
+
+        function removeClass(element, className) {
+            element.classList.remove(className);
+        }
+
+        function toggleButton(btn) {
+        
+            let btn2 = document.getElementById("boton2");
+            let x = hasClass(btn, "ventanas2");
+
+            if(btn.id == "boton")
+            {
+                btn2 = document.getElementById("boton2");
+                x = hasClass(btn, "ventanas2");
+            } else {
+                btn2 = document.getElementById("boton");
+                x = hasClass(btn, "ventanas2");
+            }
+
+            if(!x) {
+                removeClass(btn, "ventanas");
+                addClass(btn, "ventanas2");
+
+                removeClass(btn2, "ventanas2");
+                addClass(btn2, "ventanas");
+
+                console.log(btn.className);
+                console.log(btn2.className);
+            } else {
+                removeClass(btn, "ventanas2");
+                addClass(btn, "ventanas");
+
+                removeClass(btn2, "ventanas");
+                addClass(btn2, "ventanas2");
+
+                console.log(btn.className);
+                console.log(btn2.className);
+            }
+
+            console.log(x);
         }
     </script>
 </html>
